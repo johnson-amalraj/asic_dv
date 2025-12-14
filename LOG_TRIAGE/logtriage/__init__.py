88ff76d7eabd651499ef401dd43b428632b5956c
Johnson Amalraj <johnsonamalraj@outlook.com>
2025-12-14 16:03:54 +0530

    removed the logtriage folder

--- LOG_TRIAGE/logtriage/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-logtriage: A PyQt5-based log triage and visualization tool.
-"""
-
--- LOG_TRIAGE/logtriage/constants.py
+++
@@ -1,7 +0,0 @@
-MAX_RECENT_FOLDERS = 5
-RECENT_FOLDERS_KEY = "recentFolders"
-EXCLUSION_LIST_KEY = "exclusionList"
-LAST_FOLDER_KEY = "lastFolder"
-COL_WIDTHS_KEY = "colWidths"
-WIN_SIZE_KEY = "winSize"
-WIN_POS_KEY = "winPos"
